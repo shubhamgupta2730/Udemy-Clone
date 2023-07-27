2a442753fe39e960b8306d7224f2f6c5c88f9456
Shubham Gupta <shubhamgupta2730@gmail.com>
2023-07-27 18:25:33 +0530

    first commit

---
+++ build/precache-manifest.c5fffa34b75c6880a010597933060f67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3da5e6172b3bb61c7e370c5a5285764",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc6f669206c49d9903e8",
+    "url": "/static/css/main.7f23b2ee.chunk.css"
+  },
+  {
+    "revision": "ef6b13f8a3c73c54f762",
+    "url": "/static/js/2.33fc0fd2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.33fc0fd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc6f669206c49d9903e8",
+    "url": "/static/js/main.4b148281.chunk.js"
+  },
+  {
+    "revision": "d7e6632547fd919a8589",
+    "url": "/static/js/runtime-main.4fdbf3fc.js"
+  }
+]);
